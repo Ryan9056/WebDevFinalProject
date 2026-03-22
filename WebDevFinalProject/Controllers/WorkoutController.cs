1b39a6d88622afaea88488f3bf5a3afa6fb4abc3
macydebord <macyluanna@gmail.com>
2026-03-22 16:32:01 -0400

    Added WorkoutController and workout pages

---
+++ WebDevFinalProject/Controllers/WorkoutController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebDevFinalProject.Controllers
+{
+    public class WorkoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
